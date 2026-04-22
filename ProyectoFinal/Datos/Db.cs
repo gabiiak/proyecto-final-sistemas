7c34df9d6f31e51f64a4bc5815c62c5ae1ecaa0b
gabii.ak <elpollokawaiicomecoco@gmail.com>
2026-04-22 19:30:59 -0300

    creacion de sqliteconnection

---
+++ ProyectoFinal/Datos/Db.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Datos
+{
+    public class Db
+    {
+        public static SQLiteConnection GetConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SQLite"].ConnectionString;
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
