f0da935648f6460289cb90d9320d6b449d071d65
Thiago <thiagodichiara12@gmail.com>
2026-05-16 16:42:09 -0300

    Agrego DetalleVenta

---
+++ ProyectoFinal/Modelos/DetalleVenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class DetalleVenta
+    {
+        public int IdDetalleVenta { get; set; }
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public double PrecioUnitario { get; set; }
+        public double Subtotal { get; set; }
+    }
+}
